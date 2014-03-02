86d8fe28e7118a4dd136a0da3cb16d9182ba5870
admin <admin@PRORABH1>
2014-03-02 19:31:53 +0400

    02.03.2014

---
+++ src/bluetoothconnection/BluetoothConnection.java
@@ -0,0 +1,93 @@
+package bluetoothconnection;
+
+import java.io.IOException;
+import java.util.Vector;
+import javax.bluetooth.DeviceClass;
+import javax.bluetooth.DiscoveryAgent;
+import javax.bluetooth.DiscoveryListener;
+import javax.bluetooth.LocalDevice;
+import javax.bluetooth.RemoteDevice;
+import javax.bluetooth.ServiceRecord;
+import javax.microedition.io.Connection;
+
+public class BluetoothConnection {
+ 
+    LocalDevice local;
+    RemoteDevice remote;
+    DiscoveryAgent agent;
+    Connection c;
+    
+    public int devices, accessCode;
+    public static final Vector/*<RemoteDevice>*/ devicesDiscovered = new Vector();
+
+    private BluetoothConnection(){
+        
+        try {
+            // Retrieve the local Bluetooth device object
+            local = LocalDevice.getLocalDevice();
+            local.setDiscoverable(DiscoveryAgent.GIAC);
+            
+            // Retrieve the Bluetooth address of the local device
+            String address = local.getBluetoothAddress();
+            
+            // Retrieve the name of the local Bluetooth device
+            String name = local.getFriendlyName();
+            System.out.println(address +"\n "+name);
+ 
+            deviceDiscovery();
+            
+        } catch(Exception e) {
+            e.printStackTrace();
+            
+    }
+    }
+    
+    public void deviceDiscovery() throws IOException, InterruptedException {
+        
+            //retrieve the discovery agent
+            DiscoveryAgent agent = local.getDiscoveryAgent();
+            
+            final Object inquiryCompletedEvent = new Object();
+
+            devicesDiscovered.clear();
+            DiscoveryListener listener = new DiscoveryListener() {
+
+            public void deviceDiscovered(RemoteDevice btDevice, DeviceClass cod) {
+                System.out.println("Device " + btDevice.getBluetoothAddress() + " found");
+                devicesDiscovered.addElement(btDevice);
+                try {
+                    System.out.println("     name " + btDevice.getFriendlyName(false));
+                } catch (IOException cantGetDeviceName) {
+                }
+            }
+            
+            public void inquiryCompleted(int discType) {
+                System.out.println("Device Inquiry completed!");
+                synchronized(inquiryCompletedEvent){
+                    inquiryCompletedEvent.notifyAll();
+                }
+            }
+            public void serviceSearchCompleted(int transID, int respCode) { 
+            }
+
+            public void servicesDiscovered(int transID, ServiceRecord[] servRecord) {
+            }
+            };
+            
+            synchronized(inquiryCompletedEvent) {
+            boolean started = LocalDevice.getLocalDevice().getDiscoveryAgent().startInquiry(DiscoveryAgent.GIAC, listener);
+            if (started) {
+                System.out.println("wait for device inquiry to complete...");
+                inquiryCompletedEvent.wait();
+                System.out.println(devicesDiscovered.size() +  " device(s) found");
+            }
+        }   
+}
+   
+    public static void main(String[] args) {
+        
+        BluetoothConnection newconnection = new BluetoothConnection();
+        
+    }
+    
+}
---
+++ src/bluetoothconnection/RDevice.java
@@ -0,0 +1,19 @@
+//
+//
+//package bluetoothconnection;
+//
+//
+//public class RDevice {
+//    
+//    
+//    
+//    interface DiscoveryListener{
+//    
+//        DiscoveryListener.device.Discovered();
+//        DiscoveryListener.inquaryCompleted();
+//        DiscoveryListener.servicesDiscovered();
+//        DiscoveryListener.servicesSearchCompleted();
+//        
+//    }
+//    
+//}
